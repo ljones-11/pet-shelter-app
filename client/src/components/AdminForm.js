02c326d37fbe0d7720303147a4428f57fa13d25c
fedau <fedau.d@hotmail.com>
2023-01-20 15:52:19 +0000

    setting up new animal form

---
+++ client/src/components/AdminForm.js
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+
+
+
+const AdminForm = ({onCatSubmit, onDogSubmit}) => {
+
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdminForm
