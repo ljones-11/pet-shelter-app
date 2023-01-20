4ca556445fc4c4a991d1f73d3ae652eea9a8d131
sagesaintsalem <85616342+sagesaintsalem@users.noreply.github.com>
2023-01-20 16:01:48 +0000

    CATS

---
+++ client/src/containers/CatContainer.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import CatsList from '../components/CatsList';
+
+
+const CatContainer = () => {
+
+const [cats, setCats] = useState([])
+
+useEffect(() => {
+    fetchDogs();
+    fetchCats();
+}, [])
+
+
+const fetchCats = () => {
+    fetch('http://localhost:9000/api/cats')
+    .then(response => response.json())
+    .then(cats => setCats(cats))
+}
+
+
+return (
+
+    <div>
+        <CatsList cats={cats}/>
+    </div>
+)
+
+}
+
+export default CatContainer;
