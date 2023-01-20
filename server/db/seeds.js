e2ad4f307845137afd28bb971c05f284f18e0f4c
sagesaintsalem <85616342+sagesaintsalem@users.noreply.github.com>
2023-01-20 12:26:21 +0000

    Seeds

---
+++ server/db/seeds.js
@@ -0,0 +1,39 @@
+use shelter;
+db.dropDatabase();
+
+const cats = [
+    {
+    name: 'Tom',
+    breed: 'Persian',
+    age: 8,
+    about: 'Mouse botherer'
+    },
+
+    {
+    name: 'Kirby',
+    breed: 'Manx',
+    age: 6,
+    about: 'Awww his little face'
+    }
+];
+
+const dogs = [
+    {
+    name : 'Sparky',
+    breed: 'Crested Chinese',
+    age: 4,
+    about: 'He zoomin'
+    },
+
+    {
+    name: 'Cara',
+    breed: 'Afghan Hound',
+    age: 3,
+    about: 'Model bitch'
+    }
+];
+
+db.animals.insertMany(cats);
+db.animals.insertMany(dogs);
+
+
