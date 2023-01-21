30048213391521c6e8621666880c2cfd66823b58
sagesaintsalem <85616342+sagesaintsalem@users.noreply.github.com>
2023-01-21 15:39:53 +0000

    Tweaked CatsList return

--- client/src/components/CatsList.js
+++ client/src/components/CatsList.js
@@ -3,12 +3,16 @@ import CatElement from "./CatElement";
 const CatsList = ({cats}) => {
 
     const catData = cats.map((cat) => {
-        return <CatElement cat={cat} key={cat._id}/>
+        return <li>
+            <CatElement cat={cat} key={cat._id}/>
+            </li>
     })
 
 
     return (
+        <ul>
         {catData}
+        </ul>
     )
 
 }
