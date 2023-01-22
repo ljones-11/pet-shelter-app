89b3fea924aba58a22f42a8ca99db2ccefcc20a0
fedau <fedau.d@hotmail.com>
2023-01-22 10:48:56 +0000

    images added to database

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -7,6 +7,7 @@ const cats = [
     breed: 'Persian',
     age: 8,
     about: 'Mouse botherer',
+    image: 'https://images.pexels.com/photos/177809/pexels-photo-177809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     adopted: false
     },
 
@@ -15,7 +16,8 @@ const cats = [
     breed: 'Manx',
     age: 6,
     about: 'Awww his little face',
-    adopted: false
+    image: 'https://images.pexels.com/photos/4734723/pexels-photo-4734723.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    adopted: true
     },
 
     {
@@ -23,8 +25,17 @@ const cats = [
         breed: 'Russian Blue',
         age: 3,
         about: 'Hungry',
-        adopted: true
-        }
+        image: 'https://images.pexels.com/photos/590490/pexels-photo-590490.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        adopted: false
+        },
+        {
+            name: 'Ray',
+            breed: 'Sphinx',
+            age: 7,
+            about: 'Needs warmth and attention',
+            image: 'https://images.pexels.chttps://images.pexels.com/photos/991831/pexels-photo-991831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2om/photos/590490/pexels-photo-590490.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            adopted: false
+            }
 ];
 
 const dogs = [
@@ -33,6 +44,7 @@ const dogs = [
     breed: 'Crested Chinese',
     age: 4,
     about: 'He zoomin',
+    image: "https://images.pexels.com/photos/53261/pexels-photo-53261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     adopted: false
     },
 
@@ -41,6 +53,7 @@ const dogs = [
     breed: 'Afghan Hound',
     age: 3,
     about: 'Model bitch',
+    image: 'https://images.pexels.com/photos/663573/pexels-photo-663573.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     adopted: false
     },
 
@@ -49,8 +62,17 @@ const dogs = [
         breed: 'Greyhound',
         age: 2,
         about: 'Drama Queen and escape artist',
+        image: 'https://images.pexels.com/photos/11608986/pexels-photo-11608986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
         adopted: true
-        }
+        },
+        {
+            name: 'Fred',
+            breed: 'Greyhound',
+            age: 4,
+            about: 'Loves other dogs',
+            image: 'https://https://images.pexels.com/photos/1906153/pexels-photo-1906153.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2.pexels.com/photos/11608986/pexels-photo-11608986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            adopted: false
+            }
 ];
 
 const adoptions = [
