0e776e70e6def477d2864ae0c19e0adf0f2914cd
fedau <fedau.d@hotmail.com>
2023-01-23 15:20:53 +0000

    counter for open adoption requests

--- client/src/components/AdminAdoptionRequests.js
+++ client/src/components/AdminAdoptionRequests.js
@@ -10,10 +10,21 @@ const AdminAdoptionRequests = ({adoptionRequests, handleArchive}) => {
     />
 
   })
+  const total = adoptionRequests.length;
+  const archivedCounter = adoptionRequests.filter((request) =>  request.archived).length
+  const openCounter = adoptionRequests.filter((request) =>  !request.archived).length
+
+// for(var i = 0; i < adoptionRequests.adopted.length; ++i){
+//     if(adoptionRequests.adopted[i] === 'This request is still open')
+//         count++;
+// }
   return (
     <div>
         <h2>Adoption Requests</h2>
         {singleRequestNodes}
+        <h1>the total of Archived requests is {archivedCounter}</h1>
+        <h1>the total of open requests is {openCounter}</h1>
+
              
     </div>
   )
