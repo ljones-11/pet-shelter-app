fddaf043200120c423ff896182aad4eedbf9e6eb
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2023-01-24 10:14:47 +0000

    tagline styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,12 +9,19 @@ import AdminContainer from '../containers/AdminContainer';
 const NavBar = () => {
 
 const petLogo = require('../petlogo_nocolour.png')
+const logoText = require('../logo_text.png')
 
     return(
         <Router>
             <header className="header-container">
-                <h1 className='title-h1'>furFinder</h1>
-                <img className="logo-img" src={petLogo}/>
+                {/* <h1 className='title-h1'>furFinder</h1> */}
+                <div className="logo-nav-container">
+                    <div className="logo-container">
+                        <img className="logo-text-img" src={logoText}/>
+                        <em><h4 className="tagline">Swipe for a pawfect match!</h4></em>
+                    </div>
+                    <img className="logo-img" src={petLogo}/>
+                </div>
                 <nav className="nav-container">
                     <ul className="nav-ul">
                         <li>
