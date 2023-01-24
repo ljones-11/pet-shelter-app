0d94638b20cc76552708736f501f1a236075502c
fedau <fedau.d@hotmail.com>
2023-01-24 10:25:20 +0000

    setup for footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+  <footer> fotteres</footer>
+  )
+}
+
+export default Footer
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,17 +1,13 @@
 import React from "react";
 import { Link } from 'react-router-dom'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from '../containers/Homepage';
-import DogContainer from '../containers/DogContainer';
-import CatContainer from '../containers/CatContainer';
-import AdminContainer from '../containers/AdminContainer';
+
 
 const NavBar = () => {
 
 const petLogo = require('../petlogo_nocolour.png')
 
     return(
-        <Router>
+        <>
             <header className="header-container">
                 <h1 className='title-h1'>furFinder</h1>
                 <img className="logo-img" src={petLogo}/>
@@ -32,14 +28,8 @@ const petLogo = require('../petlogo_nocolour.png')
                     </ul>
                 </nav>
             </header>
-            <Routes>
-                <Route path='/' element={<Homepage />} />
-                <Route path='/dogs' element={<DogContainer />} />
-                <Route path='/cats' element={<CatContainer />} />
-                <Route path='/admin' element={<AdminContainer />} />
-            </Routes>
-            
-        </Router>
+       
+        </>
     )
 }
 
